592526842881e1595b3e686150ee065228f5ae17
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-05 23:55:55 -0300

    inserted render error get id

--- lib/rocklivery_web/views/error_view.ex
+++ lib/rocklivery_web/views/error_view.ex
@@ -22,6 +22,10 @@ defmodule RockliveryWeb.ErrorView do
     %{message: translate_errors(changeset)}
   end
 
+  def render("error.json", %{result: result}) do
+    %{message: result}
+  end
+
   def translate_errors(changeset) do
     traverse_errors(changeset, fn {msg, opts} ->
       Enum.reduce(opts, msg, fn {key, value}, acc ->
