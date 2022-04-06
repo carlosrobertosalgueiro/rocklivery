c06c1b60e436f395190a922765c77dd989af9277
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-06 00:01:51 -0300

    delegation function get user by id

--- lib/rocklivery.ex
+++ lib/rocklivery.ex
@@ -1,5 +1,7 @@
 defmodule Rocklivery do
   alias Rocklivery.Users.Create, as: UserCreate
+  alias Rocklivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
