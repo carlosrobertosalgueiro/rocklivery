9c00f002d16fed20f199626eeebf0cc15fd23b0b
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-06 14:06:51 -0300

    inserted function update

--- lib/rocklivery_web/controllers/users_controller.ex
+++ lib/rocklivery_web/controllers/users_controller.ex
@@ -26,7 +26,15 @@ defmodule RockliveryWeb.UsersController do
     with {:ok, %User{} = user} <- Rocklivery.get_user_by_id(id) do
       conn
       |> put_status(:ok)
-      |> render("show.json", user: user)
+      |> render("user.json", user: user)
+    end
+  end
+
+  def update(conn, params) do
+    with {:ok, %User{} = user} <- Rocklivery.update_user(params) do
+      conn
+      |> put_status(:ok)
+      |> render("user.json", user: user)
     end
   end
 end
