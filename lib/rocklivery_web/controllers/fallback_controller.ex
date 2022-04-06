0799bbc50216c8339c5fdb33cdb260a43247fc17
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-06 11:29:28 -0300

    refactoring error call

--- lib/rocklivery_web/controllers/fallback_controller.ex
+++ lib/rocklivery_web/controllers/fallback_controller.ex
@@ -1,9 +1,9 @@
 defmodule RockliveryWeb.FallbackController do
   use RockliveryWeb, :controller
-
+  alias Rocklivery.Error
   alias RockliveryWeb.ErrorView
 
-  def call(conn, {:error, %{status: status, result: result}}) do
+  def call(conn, {:error, %Error{status: status, result: result}}) do
     conn
     |> put_status(status)
     |> put_view(ErrorView)
