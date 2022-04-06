de123171e2d0d0d2383c1ae110ab25b030f4aa9b
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-06 01:35:22 -0300

    inserted deletion function

--- lib/rocklivery_web/controllers/users_controller.ex
+++ lib/rocklivery_web/controllers/users_controller.ex
@@ -4,13 +4,21 @@ defmodule RockliveryWeb.UsersController do
   alias Rocklivery.User
   alias RockliveryWeb.FallbackController
 
-  action_fallback FallbackController
+  action_fallback(FallbackController)
 
   def create(conn, params) do
     with {:ok, %User{} = user} <- Rocklivery.create_user(params) do
       conn
       |> put_status(:created)
       |> render("create.json", user: user)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    with {:ok, %User{}} <- Rocklivery.delete_user(id) do
+      conn
+      |> put_status(:no_content)
+      |> text("")
     end
   end
 
