67123fd43662ae49b4841b9b753e5ef0d58f4097
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-10 12:07:59 -0300

    create user

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,5 +1,7 @@
 defmodule Rocklivery.Factory do
-  use ExMachina
+  use ExMachina.Ecto, repo: Rocklivery.Repo
+
+  alias Rocklivery.User
 
   def user_params_factory do
     %{
@@ -10,6 +12,19 @@ defmodule Rocklivery.Factory do
       email: "carlosrobertosalgueiro@.com",
       password: "123465",
       name: "carlos roberto"
+    }
+  end
+
+  def user_factory do
+    %User{
+      address: "Rua das bananeiras",
+      age: 26,
+      cep: "56000000",
+      cpf: "32165498712",
+      email: "carlosrobertosalgueiro@.com",
+      password: "123465",
+      name: "carlos roberto",
+      id: "6a34b2ac-9a26-42be-90fe-4e5937618840"
     }
   end
 end
