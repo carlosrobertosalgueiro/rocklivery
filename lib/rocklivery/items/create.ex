37d8718bc3a8dd9c8878869b92c025b97c20f4ae
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-13 10:15:47 -0300

    inserted created item

---
+++ lib/rocklivery/items/create.ex
@@ -0,0 +1,16 @@
+defmodule Rocklivery.Items.Create do
+  alias Rocklivery.{Error, Item, Repo}
+
+  def call(params) do
+    params
+    |> Item.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %Item{}} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    {:error, Error.build(:bad_request, result)}
+  end
+end
