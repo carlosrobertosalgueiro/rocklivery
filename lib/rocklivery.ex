2da741dd18ea6c58ed0b464ee68555923bdb4702
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-13 11:25:41 -0300

    created delegation items

--- lib/rocklivery.ex
+++ lib/rocklivery.ex
@@ -1,4 +1,5 @@
 defmodule Rocklivery do
+  alias Rocklivery.Items.Create, as: ItemCreate
   alias Rocklivery.Users.Create, as: UserCreate
   alias Rocklivery.Users.Delete, as: UserDelete
   alias Rocklivery.Users.Get, as: UserGet
@@ -8,4 +9,5 @@ defmodule Rocklivery do
   defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
   defdelegate update_user(params), to: UserUpdate, as: :call
+  defdelegate create_item(params), to: ItemCreate, as: :call
 end
